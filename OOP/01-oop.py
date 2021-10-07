5ce38ed9a34dc75996e010ed2a8728ec513b837a
Rakesh Poduval <poduval.rakesh@icloud.com>
2021-10-07 09:00:07 +0530

    some formatting

--- OOP/01-oop.py
+++ OOP/01-oop.py
@@ -1,25 +1,38 @@
-# import data
+# initialize ----
 # %%
-import pandas as pd
 # import os
 # os.getcwd()
+import pandas as pd
 from Py.recruitment import Recruitment
+# %%
 
-# import data
+# import data ----
 # %%
 df = pd.read_csv("data/recruitment.txt", sep=";")
 df.head()
-
 # %%
-# Analysis
+
+# Analysis ----
+
+## Initiating class, print available attributes ----
+# %%
 rc = Recruitment(df)
 rc.Attributes()
-rc.df
+# %%
+
+## Attribute 1: print number of candidates ----
+# %%
+# rc.df
 rc.n_candidates
+# %%
+
+## Attribute 2: overview of candidates ----
+# %%
 rc.Overview()
+# %%
+
+## Attribute 3: Results of interview ----
+# %%
 rc.Results()
 rc.Results(top = False)
-
-
-
 # %%
