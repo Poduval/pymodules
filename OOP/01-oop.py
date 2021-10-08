6d8719f836b33beec95ee39628c35e3fd5545438
Rakesh Poduval <poduval.rakesh@icloud.com>
2021-10-09 00:00:02 +0530

    updated folder structure

--- OOP/01-oop.py
+++ OOP/01-oop.py
@@ -1,38 +1,19 @@
 # initialize ----
-# %%
-# import os
-# os.getcwd()
+import os
+os.getcwd()
 import pandas as pd
-from Py.recruitment import Recruitment
-# %%
+from OOP.recruitment import Recruitment
 
 # import data ----
-# %%
-df = pd.read_csv("data/recruitment.txt", sep=";")
+
+df = pd.read_csv("OOP/data.txt", sep=";")
 df.head()
-# %%
 
 # Analysis ----
-
-## Initiating class, print available attributes ----
-# %%
 rc = Recruitment(df)
 rc.Attributes()
-# %%
-
-## Attribute 1: print number of candidates ----
-# %%
-# rc.df
+rc.df
 rc.n_candidates
-# %%
-
-## Attribute 2: overview of candidates ----
-# %%
 rc.Overview()
-# %%
-
-## Attribute 3: Results of interview ----
-# %%
 rc.Results()
 rc.Results(top = False)
-# %%
